9170c80d7dc6672cd9a18744c893881cd497cec9
minmin <minmin0705.chen@gmail.com>
2024-07-16 21:31:55 +0800

    comment

--- lib/routes/api_connection.dart
+++ lib/routes/api_connection.dart
@@ -1,6 +1,8 @@
 class API
 {
   static const hostConnect = "http://192.168.56.1/smiley_backend";
+  //  static const hostConnect = "http://163.22.32.24/smiley_backend";
+
   static const hostConnectUser = "$hostConnect/user";
 
   // 將 firebase_uid 和其他 user 資訊放進 MySQL
